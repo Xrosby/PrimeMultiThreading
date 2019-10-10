d581974d57e2c60c9059edeb8922d179718f6b0e
Xrosby <mads.sfg@gmail.com>
2019-10-10 13:50:34 +0200

    a

---
+++ QualityAttributeAssignment/src/client/Main.java
@@ -0,0 +1,56 @@
+package client;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        HashMap<Integer, Long> noOfThreadsAndRunTime = new HashMap<>();
+	    Primes primeCalc = new Primes();
+
+
+        int maxThreads = 8;
+        for(int i = 1; i <= maxThreads; i++) {
+            System.out.println("\n_________________________________STARTING WITH " + i + " THREADS_______________________________________\n ");
+            try
+            {
+                primeCalc.calculatePrimes(i);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+           // System.out.println(primeCalc.getPrimes());
+
+            System.out.println("PRIMES: " + primeCalc.getPrimes());
+            System.out.println("\n_____________________FINISHED IN " + primeCalc.getTimeString() + " WITH " + i + " THREADS___________________\n\n" +
+                    "########################################################################\n\n");
+            noOfThreadsAndRunTime.put(i, primeCalc.getRunTime());
+        }
+
+        System.out.println("THREADS     RUNTIME");
+       noOfThreadsAndRunTime.forEach((k,v) -> {
+           int min = (int)(v/1000/60);
+           int sec = (int)(v/1000%60);
+           int milli = (int)(v%1000);
+           String runTime = min + ":" + sec + ":" + milli;
+           System.out.println(k + "            " + runTime);
+       });
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ QualityAttributeAssignment/src/client/Primes.java
@@ -0,0 +1,103 @@
+package client;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Primes
+{
+
+    private ArrayList<Integer> primes;
+    private long runTime;
+
+
+    public void calculatePrimes(int noOfThreads) throws InterruptedException
+    {
+        this.primes = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
+        int max = 500000;
+
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        int division = max / noOfThreads;
+        for (int i = 1; i <= noOfThreads; i++)
+        {
+            int threadMin = (i * division) - (division);
+            int threadMax = i * division;
+
+
+            Thread thread = new Thread("" + i)
+            {
+                @Override
+                public void run()
+                {
+                    System.out.println("Starting thread " + getName());
+                    System.out.println("Working from: " + threadMin + " to " + threadMax);
+                    startPrimeThread(threadMin, threadMax);
+                }
+            };
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread : threads)
+        {
+            thread.join();
+        }
+
+        Collections.sort(primes);
+        this.runTime = (System.currentTimeMillis() - startTime);
+
+    }
+
+
+    private void startPrimeThread(int min, int max)
+    {
+        for (int i = min; i < max; i++)
+        {
+            if (this.isPrime(i))
+            {
+                synchronized (primes)
+                {
+                    this.primes.add(i);
+                }
+                if(primes.size() % 1000 == 0) {
+                    System.out.println(primes.size());
+                }
+            }
+
+        }
+    }
+
+    public ArrayList<Integer> getPrimes()
+    {
+        return primes;
+    }
+
+    private synchronized boolean isPrime(int i)
+    {
+        boolean isPrime = true;
+
+        if (i > 2 && (i & 1) == 0) isPrime = false;
+        for (int j = 3; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public long getRunTime()
+    {
+        return this.runTime;
+    }
+
+    public String getTimeString() {
+        int min = (int)(runTime/1000/60);
+        int sec = (int)(runTime/1000%60);
+        int milli = (int)(runTime%1000);
+        String runTime = min + ":" + sec + ":" + milli;
+        return runTime;
+    }
+}
